3700cbb54937de147566643bdeca9b9c7b58a38c
이승호 <82797695+koelkorea@users.noreply.github.com>
2023-04-05 16:58:47 +0900

    Update day16-4.c
    ㅇㅇ

--- day16-4.c
+++ day16-4.c
@@ -1,60 +1,67 @@
-// - ���� �Ҵ� ���� �Լ�
+// - 동적 할당 관련 함수
 //  3. (void*) calloc (size_t _Count, size_t _size) 
-//      : _count ���� * _size �޸� ũ�� �� ������ �� �ִ� �޸� ������ �Ҵ� (���� : �޸� �����ּ� ��ȯ , ���� : NULL ��ȯ)
+//      : _count 개수 * _size 메모리 크기 를 저장할 수 있는 메모리 공간을 할당 (성공 : 메모리 시작주소 반환 , 실패 : NULL 반환)
 
-//      # (�߿�) malloc���� ������
-//        1. �Ҵ�� �޸� �� ��� 0���� �ʱ�ȭ
-//        2. ������ �Ķ���ͷ� ����, �������� �ø�
+//      # (중요) malloc과의 차이점
+//        1. 할당된 메모리 값 모두 0으로 초기화
+//        2. 개수를 파라미터로 놔서, 직관성을 늘림
 
 //  4. (void*) realloc (void* _Block, size_t size)
-//      : �̹� �Ҵ��� ������ ������ �ּҰ��� _block�ϰ�, ���Ҵ� �� ������ ũ�⸦ size�� �Է� (���� : �޸� �����ּ� ��ȯ , ���� : NULL ��ȯ)
+//      : 이미 할당한 포인터 변수의 주소값을 _block하고, 재할당 할 공간의 크기를 size에 입력 (성공 : 메모리 시작주소 반환 , 실패 : NULL 반환)
 
 #include<stdio.h>
 #include<stdlib.h>              // malloc(), free()
 #include<math.h>
-#include<stdlib.h>				// srand(), rand() �Լ� ��� �����ϰ� ��
+#include<stdlib.h>				// srand(), rand() 함수 사용 가능하게 함
 
-// N���� �л� ���� �Է� & ��� �ڵ带 �ۼ��ϴ� ���� (�����Ҵ��� ������!)
+// N명의 학생 성적 입력 & 출력 코드를 작성하는 문제 (동적할당을 쓰도록!)
 int main(void) {
     srand((unsigned int)time(NULL));
 
-    // ���� n : �л��� ��, start_address : ���� �Է��� �ּҰ� ���� �༮
+    // 변수 n : 학생의 수, start_address : 점수 입력할 주소값 받을 녀석
     int n = 0, size = 0;
     int* start_address = 0;
 
+    // 평균, 편차, 분산, 표준편차
     float average = 0, deviation = 0, variance = 0, standard_deviation = 0;
 
-    printf("������ �Է��� �л��� �� ���ΰ�? : ");
+    printf("성적을 입력할 학생은 몇 명인가? : ");
     scanf_s("%d", &n);
 
-    start_address =  (int*) calloc(n, sizeof(int));
+    // int n개 메모리 할당, 그 시작 주소를 int 포인터변수 start_address에 반환
+    //  -> 이를 통해 구조체 배열 int start_address[n]을 동적으로 선언한 효과를 냄
+    
+    // (void*) calloc (size_t _Count, size_t _size) 
+    //  : _count 개수 * _size 메모리 크기 를 저장할 수 있는 메모리 공간을 할당 (성공 : 메모리 시작주소 반환 , 실패 : NULL 반환)
+    start_address =  (int*) calloc(n, sizeof(int));  // int 포인터변수에 해당하는 만큼의 메모리를 n개 연속으로 동적 할당한 후, 그 시작 주소값을 int 포인터변수 start_address 반환 + 메모리값들 모두를 0으로 초기화
 
     size = _msize(start_address);
 
-    // _msize() : �����Ͱ� ����Ű�� �޸� ũ�⸦ ��ȯ�ϴ� �Լ�
-    printf("�Ҵ�� �޸� : %d \n", size);
+    // _msize() : 포인터가 가리키는 메모리 크기를 반환하는 함수
+    printf("할당된 메모리 : %d \n", size);
 
+    // 동적할당된 주소값이 저장된 start_address를 역참조하여 학생들 성적 입력
     for (int i = 0; i < n; i++) {
         *(start_address + i) = rand() % 101;
-        printf("�л� %d : %d\n", i + 1, *(start_address + i));
+        printf("학생 %d : %d\n", i + 1, *(start_address + i));
 
-        average += (float) *(start_address + i);
+        // 평균 구하기
+        average += ((float) *(start_address + i) / n);
     }
 
-    average /= n;
-
+    // 같은 원리로 편차와 분산 구하기
     for (int i = 0; i < n; i++) {
 
         deviation = (float)(*(start_address + i) - average);
-        variance += (float)( *(start_address + i) - average) * (*(start_address + i) - average);
+        variance += (deviation * deviation / n);
     }
-
-    variance /= n;
+    
+    // 표준편차 구하기
     standard_deviation = sqrt(variance);
 
-    printf("��� : %3.6f�� \n", average);
-    printf("�л� : %3.6f�� \n", variance);
-    printf("ǥ������ : %3.6f�� \n", standard_deviation);
+    printf("평균 : %3.6f점 \n", average);
+    printf("분산 : %3.6f점 \n", variance);
+    printf("표준편차 : %3.6f점 \n", standard_deviation);
 
 
     return 0;
